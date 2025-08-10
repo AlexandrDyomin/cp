7da96e0953bc59459dd6f559eaa309d17c6291bf
Александр <alexandr.dyomin91@gmail.com>
2025-08-10 17:57:02 +0900

    build

--- transactions.c7f047e9.js
+++ transactions.c7f047e9.js
@@ -2,22 +2,22 @@ function t(t,e,a,n){Object.defineProperty(t,e,{get:a,set:n,enumerable:!0,configu
             <nav class="navigation">
                 <ul class="navigation__list">
                     <li>
-                        <a class="navigation__link" href="/" title="\u{41A}\u{43E}\u{448}\u{435}\u{43B}\u{435}\u{43A}">
+                        <a class="navigation__link" href="/cp/" title="\u{41A}\u{43E}\u{448}\u{435}\u{43B}\u{435}\u{43A}">
                             <img class="navigation__img" src="${"/cp"+new URL(r("1df7L"))}" alt="\u{41A}\u{43E}\u{448}\u{435}\u{43B}\u{451}\u{43A}">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/transactions" title="\u{422}\u{440}\u{430}\u{43D}\u{437}\u{430}\u{43A}\u{446}\u{438}\u{438}">
+                        <a class="navigation__link" href="/cp/transactions" title="\u{422}\u{440}\u{430}\u{43D}\u{437}\u{430}\u{43A}\u{446}\u{438}\u{438}">
                             <img class="navigation__img" src="${"/cp"+new URL(r("6Vp7X"))}" alt="\u{411}\u{443}\u{445}\u{433}\u{430}\u{43B}\u{442}\u{435}\u{440}\u{441}\u{43A}\u{430}\u{44F} \u{43A}\u{43D}\u{438}\u{433}\u{430}">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/realizedPnL" title="\u{420}\u{435}\u{430}\u{43B}\u{438}\u{437}\u{43E}\u{432}\u{430}\u{43D}\u{43D}\u{44B}\u{439} PnL">
+                        <a class="navigation__link" href="/cp/realizedPnL" title="\u{420}\u{435}\u{430}\u{43B}\u{438}\u{437}\u{43E}\u{432}\u{430}\u{43D}\u{43D}\u{44B}\u{439} PnL">
                             <img class="navigation__img" src="${"/cp"+new URL(r("imq0f"))}" alt="\u{420}\u{443}\u{43A}\u{430} \u{441} \u{43C}\u{435}\u{448}\u{435}\u{447}\u{43A}\u{43E}\u{43C} \u{434}\u{43B}\u{44F} \u{434}\u{435}\u{43D}\u{435}\u{433}">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/unrealizedPnL" title="\u{41D}\u{435}\u{440}\u{435}\u{430}\u{43B}\u{438}\u{437}\u{43E}\u{432}\u{430}\u{43D}\u{43D}\u{44B}\u{439} PnL">
+                        <a class="navigation__link" href="/cp/unrealizedPnL" title="\u{41D}\u{435}\u{440}\u{435}\u{430}\u{43B}\u{438}\u{437}\u{43E}\u{432}\u{430}\u{43D}\u{43D}\u{44B}\u{439} PnL">
                             <img class="navigation__img" src="${"/cp"+new URL(r("3BkXQ"))}" alt="\u{413}\u{438}\u{441}\u{442}\u{43E}\u{433}\u{440}\u{430}\u{43C}\u{43C}\u{430}">
                         </a>
                     </li>
