e210914f9fd8587e4bbc18a371e12e5c97d0edb4
Александр <alexandr.dyomin91@gmail.com>
2025-08-10 17:40:37 +0900

    Рефакторинг

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -5,30 +5,32 @@ customElements.define('custom-nav', class extends HTMLElement {
             let li = this.querySelector(`li:nth-child(${activeLink})`);
             li.className = 'navigation__list-item_active';
         }, 0);
+        let dir = (process.env.NODE_ENV === 'production') ? '/cp' : '';
         this.innerHTML = `
             <nav class="navigation">
                 <ul class="navigation__list">
                     <li>
                         <a class="navigation__link" href="/" title="Кошелек">
-                            <img class="navigation__img" src="${ new URL('wallet.png', import.meta.url) }" alt="Кошелёк">
+                            <img class="navigation__img" src="${ dir + new URL('wallet.png', import.meta.url) }" alt="Кошелёк">
                         </a>
                     </li>
                     <li>
                         <a class="navigation__link" href="/transactions" title="Транзакции">
-                            <img class="navigation__img" src="${ new URL('ledger.png', import.meta.url) }" alt="Бухгалтерская книга">
+                            <img class="navigation__img" src="${ dir + new URL('ledger.png', import.meta.url) }" alt="Бухгалтерская книга">
                         </a>
                     </li>
                     <li>
                         <a class="navigation__link" href="/realizedPnL" title="Реализованный PnL">
-                            <img class="navigation__img" src="${ new URL('profit.png', import.meta.url) }" alt="Рука с мешечком для денег">
+                            <img class="navigation__img" src="${ dir + new URL('profit.png', import.meta.url) }" alt="Рука с мешечком для денег">
                         </a>
                     </li>
                     <li>
                         <a class="navigation__link" href="/unrealizedPnL" title="Нереализованный PnL">
-                            <img class="navigation__img" src="${ new URL('analytics.png', import.meta.url) }" alt="Гистограмма">
+                            <img class="navigation__img" src="${ dir + new URL('analytics.png', import.meta.url) }" alt="Гистограмма">
                         </a>
                     </li>
                 </ul>
-            </nav>`
+            </nav>
+        `
     }
 })
