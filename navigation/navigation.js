a2eb8dd1cf8796504a66d2c155f7566ec2a92857
Александр <alexandr.dyomin91@gmail.com>
2025-08-10 17:54:38 +0900

    Рефакторинг

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -10,22 +10,22 @@ customElements.define('custom-nav', class extends HTMLElement {
             <nav class="navigation">
                 <ul class="navigation__list">
                     <li>
-                        <a class="navigation__link" href="/" title="Кошелек">
+                        <a class="navigation__link" href="${dir}/" title="Кошелек">
                             <img class="navigation__img" src="${ dir + new URL('wallet.png', import.meta.url) }" alt="Кошелёк">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/transactions" title="Транзакции">
+                        <a class="navigation__link" href="${dir}/transactions" title="Транзакции">
                             <img class="navigation__img" src="${ dir + new URL('ledger.png', import.meta.url) }" alt="Бухгалтерская книга">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/realizedPnL" title="Реализованный PnL">
+                        <a class="navigation__link" href="${dir}/realizedPnL" title="Реализованный PnL">
                             <img class="navigation__img" src="${ dir + new URL('profit.png', import.meta.url) }" alt="Рука с мешечком для денег">
                         </a>
                     </li>
                     <li>
-                        <a class="navigation__link" href="/unrealizedPnL" title="Нереализованный PnL">
+                        <a class="navigation__link" href="${dir}/unrealizedPnL" title="Нереализованный PnL">
                             <img class="navigation__img" src="${ dir + new URL('analytics.png', import.meta.url) }" alt="Гистограмма">
                         </a>
                     </li>
