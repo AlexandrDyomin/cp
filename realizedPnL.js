0667f55e4893ffe3e481111d639a8649b96e544d
Александр <alexandr.dyomin91@gmail.com>
2025-10-11 21:19:28 +0900

    рефакторинг

--- realizedPnL.js
+++ realizedPnL.js
@@ -1,7 +1,6 @@
 import './navigation/navigation.js';
 import './download_btn.js';
 import { getTradingStatistics } from './getTradingStatistics.js';
-import { renderRows } from './renderRows.js';
 import { CustomPnLRow } from './Pairs_row.js';
 
 let table = document.querySelector('.pairs');
@@ -9,20 +8,16 @@ let table = document.querySelector('.pairs');
 (async () => {
     // получим анализ торговли
     let data = await getTradingStatistics();
-    
-    Promise.all(data)
-        .then((statistics) => {
-            // отсортируем statistics по убыванию прибыли
-            let sortedStatistics = [...statistics].sort((a, b) => 
-                b.realized.invested - a.realized.invested
-            )
-            // отобразим результат
-            renderRows(
-                table, 
-                sortedStatistics.filter((item) => item.realized.invested), 
-                (item) => new CustomPnLRow({ pair: item.pair, ...item.realized })
-            );
+    // отобразим результат
+    for (let pair of data) {
+        pair
+        .then((data) => {
+            if (!data.realized.invested) {
+                return
+            }
+            table.append( new CustomPnLRow({ pair: data.pair, ...data.realized }));
         })
+    }
 })();
 
 
--- unrealized.js
+++ unrealized.js
@@ -1,7 +1,6 @@
 import './navigation/navigation.js';
 import './download_btn.js';
 import { getTradingStatistics } from './getTradingStatistics.js';
-import { renderRows } from './renderRows.js';
 import { CustomPnLRow } from './Pairs_row.js';
 
 let table = document.querySelector('.pairs');
@@ -9,18 +8,14 @@ let table = document.querySelector('.pairs');
 (async () => {
     // получим анализ торговли
     let data = await getTradingStatistics();
-    
-    Promise.all(data)
-        .then((statistics) => {
-            // отсортируем statistics по убыванию прибыли
-            let sortedStatistics = [...statistics].sort((a, b) => 
-                b.unrealized.invested - a.unrealized.invested
-            )
-            // отобразим результат
-            renderRows(
-                table, 
-                sortedStatistics.filter((item) => item.unrealized.invested), 
-                (item) => new CustomPnLRow({ pair: item.pair, ...item.unrealized })
-            );
+    // отобразим результат
+    for (let pair of data) {
+        pair
+        .then((data) => {
+            if (!data.unrealized.invested) {
+                return
+            }
+            table.append( new CustomPnLRow({ pair: data.pair, ...data.unrealized }));
         })
+    }
 })();
